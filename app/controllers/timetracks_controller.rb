2db2295372c7d318d50d35c02628254943755d8b
hedgarbezerra <hedgarbezerra35@gmail.com>
2019-04-12 12:38:22 -0300

    Bug fixes and CSS changes

--- app/controllers/timetracks_controller.rb
+++ app/controllers/timetracks_controller.rb
@@ -7,7 +7,7 @@ class TimetracksController < ApplicationController
 
     if  params[:search].present?
       @search = params[:search]
-      @timetracks = Timetrack.where("id LIKE :search OR user_id LIKE :search", search: "#{@search}")
+      @timetracks = Timetrack.where("id = :search OR user_id = :search", search: "#{@search}")
     
     else 
        @timetracks = Timetrack.all
